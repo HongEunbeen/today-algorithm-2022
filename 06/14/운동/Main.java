7dede8cde24d0389a7c68c4ad1f90ff2e808759a
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2022-06-25 13:52:58 +0900

    [0614] 운동

---
+++ 06/14/운동/Main.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+class Max {
+    private final int max = 987654321;
+    public int getMax(){
+        return max;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Max max = new Max();
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[V + 1][V + 1];
+
+        for (int i = 1; i <= V; i++) {
+            for (int j = 1; j <= V; j++) {
+                if (i != j) {
+                    arr[i][j] = max.getMax();
+                }
+            }
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            arr[a][b] = c;
+        }
+
+        for (int k = 1; k <= V; k++) {
+            for (int i = 1; i <= V; i++) {
+                for (int j = 1; j <= V; j++) {
+                    if (i == j) {
+                        continue;
+                    }
+
+                    if (arr[i][j] > arr[i][k] + arr[k][j]) {
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                    }
+                }
+            }
+        }
+
+        int ans = max.getMax();
+        for (int i = 1; i <= V; i++) {
+            for (int j = 1; j <= V; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                if (arr[i][j] != max.getMax() && arr[j][i] != max.getMax()) {
+                    ans = Math.min(ans, arr[i][j] + arr[j][i]);
+                }
+            }
+        }
+
+        System.out.println((ans == max.getMax()) ? -1 : ans);
+        br.close();
+    }
+}
