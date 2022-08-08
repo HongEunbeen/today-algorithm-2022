c5109b41b55361a48d182e218851f92905b0c82e
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2022-08-08 17:05:28 +0900

    [0808] 특정한 최단경로

--- 08/08/특정한_최단경로/Main.java
+++ 08/08/특정한_최단경로/Main.java
@@ -20,7 +20,6 @@ class Edge implements Comparable<Edge> {
 public class Main {
     static int N, E, SUM;
     static List<Edge>[] list;
-    static int[] dp;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -29,8 +28,7 @@ public class Main {
         E = Integer.parseInt(st.nextToken()); // 간선 개수
 
         list = new ArrayList[N+1];
-        dp = new int[N+1];
-
+        
         for(int i=1; i<=N; i++){
             list[i] = new ArrayList<>();
         }
@@ -50,24 +48,25 @@ public class Main {
         int v1 = Integer.parseInt(st.nextToken()); // 반드시 거쳐야 하는 정점 1
         int v2 = Integer.parseInt(st.nextToken()); // 반드시 거쳐야 하는 정점 2
 
-        Dijkstra(1);
+        int start1[] = Dijkstra(1);
 
-        int sum1 = dp[v1]; // 1-> v1
-        int sum2 = dp[v2]; // 1-> v2
+        int sum1 = start1[v1]; // 1-> v1
+        int sum2 = start1[v2]; // 1-> v2
 
-        Dijkstra(v1);
-        sum1 += dp[v2]; // v1 -> v2
-        sum2 += dp[N]; // v1 -> N
+        int start2[] = Dijkstra(v1);
+        sum1 += start2[v2]; // v1 -> v2
+        sum2 += start2[N]; // v1 -> N
 
-        Dijkstra(v2);
-        sum2 += dp[v1];// v2 -> v1
-        sum1 += dp[N];// v2 -> N
+        int start3[] = Dijkstra(v2);
+        sum1 += start3[N];// v2 -> N
+        sum2 += start3[v1];// v2 -> v1
 
         System.out.println(Math.min(sum1, sum2) == Integer.MAX_VALUE ? -1 : Math.min(sum1, sum2));
     }
 
-    static void Dijkstra(int index){
+    static int[] Dijkstra(int index){
         PriorityQueue<Edge> queue = new PriorityQueue<>();
+        int[] dp = new int[N+1];
         Arrays.fill(dp, Integer.MAX_VALUE);
 
         dp[index] = 0;
@@ -94,5 +93,7 @@ public class Main {
                 }
             }
         }
+
+        return dp;
     }
 }
